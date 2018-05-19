acf3537c8da4734fedde28193b2f48b462f26af0
monika <lmounika46@gmail.com>
2018-05-19 11:24:17 -0400

    Update the controller and add setvices to the startup.cs file

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
-            return View();
+            _todoItemService = todoItemService;
         }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
+        }
+
     }
 }
+
--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,8 @@ namespace AspNetCoreTodo
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.AddSingleton<ITodoItemService, FakeTodoItemServices>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
